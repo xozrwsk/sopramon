757b0c5123768050e43810446e1a8dffd9a844b4
Unknown <audeveniere@outlook.fr>
2018-08-16 17:20:56 +0200

    vaude

--- sopramon-html/WebContent/connexion-jquery.js
+++ sopramon-html/WebContent/connexion-jquery.js
@@ -1,29 +1,24 @@
 
-function appenConnexion() {
+$('form').bind('submit', function() {
 	let myConnexion = {
-		usernameCo: $('input[name="usernameCo"]').val(),
-		passwordCo: $('input[name="passwordCo"]').val(),
-	};
+		username : $('input[name="usernameCo"]').val(),
+		password : $('input[name="passwordCo"]').val()
+
 	};
 
-function addConnexion(){
+	
+	$.ajax({
+		method : 'POST',
+		url : 'http://192.168.1.100:8080/sopramon-web/api/auth',
+		data : myConnexion,
 
-$('sumbit').bind('click', function() {
-$.ajax({
-	method: 'POST',
-	url: 'http://192.168.1.100:8080/sopramon-web/api/auth',
-	 type : "POST",
-	data: {
-	attr: 'usernameCo',
-	attr2: 'passwordCo'
-	},
-	success: function(myConnexion) {
-		console.log(myConnexion);
-		 
-		alert("Vous êtes bien connecté" + usernameCo); 
-	}
+		success : function(myConnexion) {
+			console.log(myConnexion);
+
+			alert("Vous êtes bien connecté" + username);
+
+		}
 	});
+
+	return false;
 });
-}	
-
-
