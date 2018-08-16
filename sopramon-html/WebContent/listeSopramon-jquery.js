2167a334761c949cac6fa7d9a694f538a26b9a48
Unknown <xavier.ozarowski.14@eigsi.fr>
2018-08-16 16:43:05 +0200

    liste sopramons

--- sopramon-html/WebContent/listeSopramon-jquery.js
+++ sopramon-html/WebContent/listeSopramon-jquery.js
@@ -8,14 +8,14 @@ function appendSopramon(sopramon) {
 	
 	// Affecter le contenu à mes colonnes
 	myColNom.html(sopramon.nom);
-	myColDateNais.html(sopramon.dateNais);
+	myColDateNaissance.html(sopramon.dateNais);
 	myColExperience.html(sopramon.experience);
 	myColNiveau.html(sopramon.niveau);
 	myColArgent.html(sopramon.argent);
 	
 	// Donner les colonnes à la ligne
 	myRow.append(myColNom);
-	myRow.append(myColDateNais);
+	myRow.append(myColDateNaissance);
 	myRow.append(myColExperience);
 	myRow.append(myColNiveau);
 	myRow.append(myColArgent);
@@ -53,3 +53,23 @@ $.ajax({
 })
 
 });
+
+//let mySopramon = {
+//	id : $('input[name="id"]').val(),
+//	nom : $('input[name="nom"]').val(),
+//	prenom : $('input[name="prenom"]').val(),
+//	username : $('input[name="username"]').val(),
+//	password : $('input[name="password"]').val(),
+//	dateNaissance : $('input[name="dateNaissance"]').val(),
+//	signe : $('input[name="signe"]').val(),
+//	experience : $('input[name="experience"]').val(),
+//	niveau : $('input[name="niveau"]').val(),
+//	argent : $('input[name="argent"]').val()
+//};
+//
+//$.ajax({
+//	method : 'GET',
+//	url : 'http://192.168.1.100:8080/sopramon-web/api/sopramons',
+//	contentType : 'application/json',
+//	data : JSON.stringify(mySopramon)
+//});
