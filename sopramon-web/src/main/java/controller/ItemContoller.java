e10f564ae6fe9d89857a6ba3680fb96a3831dd76
Unknown <xavier.ozarowski.14@eigsi.fr>
2018-08-30 17:21:52 +0200

    gestion items

--- sopramon-web/src/main/java/controller/ItemContoller.java
+++ sopramon-web/src/main/java/controller/ItemContoller.java
@@ -2,6 +2,8 @@ package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import sopramon.dao.IDAOItem;
 
@@ -10,5 +12,11 @@ public class ItemContoller {
 	
 	@Autowired
 	private IDAOItem daoItem;
+	
+	@GetMapping("/gestionItems")
+	public String listerProd(Model model) {
+		model.addAttribute("items", daoItem.findAll());
+	return "gestionItems";
+	}
 
 }
