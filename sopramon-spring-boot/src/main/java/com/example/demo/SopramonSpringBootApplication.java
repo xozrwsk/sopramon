a0412f8a4e73d1cbb18bbbf36b47f3ee3a7b3dc2
Unknown <xavier.ozarowski.14@eigsi.fr>
2018-09-05 11:52:06 +0200

    spring boot

---
+++ sopramon-spring-boot/src/main/java/com/example/demo/SopramonSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopramonSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopramonSpringBootApplication.class, args);
+	}
+}
