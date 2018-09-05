ccd2456f8742c2263b6374a719cdb547dd59b7f5
Unknown <audeveniere@outlook.fr>
2018-09-05 14:15:29 +0200

    der version

--- sopramon-spring-boot/src/main/java/sopramonboot/SopramonSpringBootApplication.java
+++ sopramon-spring-boot/src/main/java/sopramonboot/SopramonSpringBootApplication.java
@@ -2,8 +2,10 @@ package sopramonboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("model")
 public class SopramonSpringBootApplication {
 
 	public static void main(String[] args) {
