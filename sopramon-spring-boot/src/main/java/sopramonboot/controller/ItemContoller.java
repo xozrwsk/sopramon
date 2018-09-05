0547a30eb79e58d61028a2045b3a7adcf4178836
Unknown <audeveniere@outlook.fr>
2018-09-05 15:59:44 +0200

    la fin !

---
+++ sopramon-spring-boot/src/main/java/sopramonboot/controller/ItemContoller.java
@@ -0,0 +1,83 @@
+package sopramonboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import sopramonbis.model.Item;
+import sopramonboot.dao.IDAOItem;
+
+@Controller
+public class ItemContoller {
+
+	@Autowired
+	private IDAOItem daoItem;
+
+	@GetMapping("/gestionItems")
+	public String listerItem(Model model) {
+		model.addAttribute("items", daoItem.findAll());
+		return "gestionItems";
+	}
+
+	@GetMapping("/modifierItem")
+	public String modifierItem(@RequestParam int id, Model model) {
+		model.addAttribute("item", daoItem.findById(id).get());
+		return "modifierItem";
+	}
+
+	@PostMapping("/modifierItem")
+	public String modifierItem(@RequestParam String nom, @RequestParam int prix, @RequestParam int attaque,
+			@RequestParam int defense, @RequestParam int esquive, @RequestParam int vitesse, @RequestParam int id,
+			@RequestParam String description, Model model) {
+		Item myItem = new Item();
+
+		myItem.setId(id);
+		myItem.setNom(nom);
+		myItem.setPrix(prix);
+		myItem.setAttaque(attaque);
+		myItem.setDefense(defense);
+		myItem.setEsquive(esquive);
+		myItem.setVitesse(vitesse);
+		myItem.setDescription(description);
+
+		daoItem.save(myItem);
+		return "redirect:/gestionItems";
+	}
+
+	@GetMapping("/supprimerItem")
+	public String supprimerItem(@RequestParam int id, Model model) {
+		Item myItem = new Item();
+		myItem.setId(id);
+
+		daoItem.deleteById(id);
+		return "redirect:/gestionItems";
+	}
+
+	@GetMapping("/creerItem")
+	public String creerItem(Model model) {
+		model.addAttribute("items", daoItem.findAll());
+		return "creerItem";
+	}
+
+	@PostMapping("/creerItem")
+	public String creerItem(@RequestParam String nom, @RequestParam int prix, @RequestParam int attaque,
+			@RequestParam int defense, @RequestParam int esquive, @RequestParam int vitesse,
+			@RequestParam String description, Model model) {
+		Item myItem = new Item();
+
+		myItem.setNom(nom);
+		myItem.setPrix(prix);
+		myItem.setAttaque(attaque);
+		myItem.setDefense(defense);
+		myItem.setEsquive(esquive);
+		myItem.setVitesse(vitesse);
+		myItem.setDescription(description);
+
+		daoItem.save(myItem);
+		return "redirect:/gestionItems";
+	}
+
+}
---
+++ sopramon-spring-boot/src/main/java/sopramonboot/controller/SopramonController.java
@@ -0,0 +1,23 @@
+package sopramonboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import sopramonboot.dao.IDAOSopramon;
+
+
+@Controller
+public class SopramonController {
+	
+	@Autowired
+	private IDAOSopramon daoSopramon;
+	
+	@GetMapping("/listeSopramon")
+	public String listerProd(Model model) {
+		model.addAttribute("sopramons", daoSopramon.findAll());
+	return "listeSopramon";
+	}
+
+}
